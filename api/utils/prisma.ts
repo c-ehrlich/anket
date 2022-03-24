47ad93db1d51d492af40c4d6903afa9f7bb64c1e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-24 22:38:40 +0100

    REBASEME prisma works

---
+++ api/utils/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
+export default prisma;
---
+++ prisma/migrations/20220324213027_dev2/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - The primary key for the `accounts` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `sessions` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "user_id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "accounts_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "accounts_id_seq";
+
+-- AlterTable
+ALTER TABLE "sessions" DROP CONSTRAINT "sessions_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "user_id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "sessions_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "sessions_id_seq";
+
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "users_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
