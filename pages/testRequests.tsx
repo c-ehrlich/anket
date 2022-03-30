f66f1dacd0c0b2f6c6e2d88c0f3c4c09fe469ff3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-30 19:12:49 +0200

    Move TestRequests to their own component

---
+++ pages/testRequests.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MakeRequest from '../components/MakeRequest'
+
+const testRequests = () => {
+  return (
+    <MakeRequest />
+  )
+}
+
+export default testRequests
