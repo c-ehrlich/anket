28a3e984c868979632669e8129798a165bfa306c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-18 15:30:42 +0900

    create /survey/mine

---
+++ pages/survey/mine.tsx
@@ -0,0 +1,26 @@
+import { Stack, Text, Title } from '@mantine/core';
+import React from 'react'
+import SurveyHero from '../../components/SurveyHero';
+import useMySurveys from '../../hooks/useMySurveys';
+
+const AllSurveys = () => {
+  const {data: surveys, status} = useMySurveys();
+
+  if (status === 'loading') {
+    return (<div>Loading...</div>)
+  }
+
+  if (status === 'error') {
+    return (<div>{status}</div>)
+  }
+
+  return (
+    <Stack>
+      <Title order={2}>All Surveys</Title>
+      <Text>tktk add sorting/filtering here?</Text>
+      {surveys && surveys.map(survey => <SurveyHero survey={survey} key={survey.id} />)}
+    </Stack>
+  )
+}
+
+export default AllSurveys;
