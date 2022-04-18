2c4f25c2ac48a44fb6e4615ae2b4170d1f8ee79e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-18 13:09:45 +0900

    create multipleChoiceOptionService

---
+++ api/multipleChoiceOption/multipleChoiceOption.service.ts
@@ -0,0 +1,67 @@
+import log from '../utils/logger';
+import prisma from '../utils/prisma';
+
+export async function createDefaultMultipleChoiceOption({
+  questionId,
+}: {
+  questionId: string;
+}) {
+  try {
+    const multipleChoiceOption = prisma.multipleChoiceOption.create({
+      data: { name: '', questionId },
+    });
+
+    return multipleChoiceOption;
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function updateMultipleChoiceOptionName({
+  id,
+  name,
+}: {
+  id: string;
+  name: string;
+}) {
+  try {
+    const multipleChoiceOption = prisma.multipleChoiceOption.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return multipleChoiceOption;
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function deleteMultipleChoiceOption({ id }: { id: string }) {
+  try {
+    const deletedOption = prisma.multipleChoiceOption.delete({ where: { id } });
+
+    return deletedOption;
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function deleteMultipleChoiceOptionsForQuestion({
+  questionId,
+}: {
+  questionId: string;
+}) {
+  try {
+    const deletedOptions = prisma.multipleChoiceOption.deleteMany({
+      where: { questionId },
+    });
+
+    return deletedOptions;
+  } catch (e) {
+    log.error(e);
+  }
+}
