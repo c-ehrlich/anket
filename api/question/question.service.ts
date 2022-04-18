9cad63011d86e089e2b15271551718dd8e38fa41
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-18 13:09:30 +0900

    Create questionService

---
+++ api/question/question.service.ts
@@ -0,0 +1,81 @@
+import { Question, QuestionType } from '@prisma/client';
+import _logger from 'next-auth/lib/logger';
+import log from '../utils/logger';
+import prisma from '../utils/prisma';
+
+export async function createDefaultQuestion({
+  surveyId,
+}: {
+  surveyId: string;
+}) {
+  try {
+    const question = prisma.question.create({
+      data: {
+        surveyId,
+        question: '',
+        details: '',
+        questionType: 'multipleChoiceMultiple',
+      },
+    });
+    return question;
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function updateQuestion({
+  id,
+  question,
+  details,
+  isRequired,
+  questionType,
+}: {
+  id: string;
+  question?: string;
+  details?: string;
+  isRequired?: boolean;
+  questionType?: QuestionType;
+}) {
+  // update stuff
+  try {
+    const updatedQuestion = prisma.question.update({
+      where: { id },
+      data: {
+        question,
+        details,
+        isRequired,
+        questionType,
+      },
+    });
+
+    // delete
+    if (
+      !(
+        questionType === 'multipleChoiceMultiple' ||
+        questionType === 'multipleChoiceSingle'
+      )
+    ) {
+      prisma.multipleChoiceOption.deleteMany({
+        where: {
+          questionId: id,
+        },
+      });
+    }
+
+    return updatedQuestion;
+  } catch (e) {
+    log.error(e);
+  }
+}
+
+export async function deleteQuestion({ id }: { id: string }) {
+  try {
+    const deletedQuestion = prisma.question.delete({
+      where: { id },
+    });
+
+    return deletedQuestion
+  } catch (e) {
+    log.error(e);
+  }
+}
