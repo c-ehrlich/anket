7e6c5db179ac497b9569dc50089f588431e46f18
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-19 16:13:59 +0900

    feat: Add edit link to SurveyHero

--- components/SurveyHero.tsx
+++ components/SurveyHero.tsx
@@ -1,4 +1,5 @@
 import {
+  ActionIcon,
   Avatar,
   Card,
   Group,
@@ -8,14 +9,18 @@ import {
   Title,
   useMantineTheme,
 } from '@mantine/core';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 import { SurveyWithAuthor } from '../types/survey';
+import { Edit } from 'tabler-icons-react';
+import Link from 'next/link';
 
 type Props = {
   survey: SurveyWithAuthor;
 };
 
 const SurveyHero = (props: Props) => {
+  const { data: session } = useSession();
   const theme = useMantineTheme();
 
   return (
@@ -24,8 +29,23 @@ const SurveyHero = (props: Props) => {
         <Image src='https://picsum.photos/600/300' height={160} alt='Norway' />
       </Card.Section>
       <Stack style={{ marginTop: theme.spacing.sm }}>
-        <Title order={3}>{props.survey.name}</Title>
-        <Text>{props.survey.description}</Text>
+        <Group style={{ justifyContent: 'space-between' }}>
+          <Title order={3}>
+            {props.survey.name !== '' ? props.survey.name : '(no name)'}
+          </Title>
+          {props.survey.author.id === session?.user?.id && (
+            <Link href={`/survey/edit/${props.survey.id}`} passHref>
+              <ActionIcon>
+                <Edit />
+              </ActionIcon>
+            </Link>
+          )}
+        </Group>
+        <Text>
+          {props.survey.description !== ''
+            ? props.survey.description
+            : '(no description)'}
+        </Text>
         <Group>
           <Avatar src={props.survey.author.image} radius='xl' />
           <Text size='sm' lineClamp={2} weight={500}>
