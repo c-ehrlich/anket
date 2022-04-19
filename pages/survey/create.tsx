0c6760e295ccbc3b0a868ed8ec940a265c21abb2
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-19 14:28:00 +0900

    refactor: Separate creation and editing of surveys

--- pages/survey/create.tsx
+++ pages/survey/create.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import CreateSurvey from '../../components/CreateSurvey'
 import CreateSurveyRQ from '../../components/CreateSurveyRQ'
+
+/**
+ * This is just a dummy page that awaits the creation of a survey (or 
+ * the fetching of an in progress survey) and then redirects to the edit
+ * page for that survey once it knows the id 
+ */
 
 const create = () => {
   return (
