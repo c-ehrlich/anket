310ec18d8d0d076f5414ccc1106915f22e5e02f3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-19 14:30:33 +0900

    (temp) fix: remove validateResource

--- pages/api/survey/index.ts
+++ pages/api/survey/index.ts
@@ -1,12 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import NextConnectHandler from 'next-connect';
 import requireSession from '../../../api/middleware/requireSession.middleware';
-import validateResource from '../../../api/middleware/validateResource.middleware';
 import { createNewSurveyHandler, getAllSurveysHandler } from '../../../api/survey/survey.controller';
-import { createDefaultSurveySchema } from '../../../api/survey/survey.schema';
 
 const handler = NextConnectHandler<NextApiRequest, NextApiResponse>()
   .get(requireSession, getAllSurveysHandler)
-  .post(validateResource(createDefaultSurveySchema), requireSession, createNewSurveyHandler);
+  .post(requireSession, createNewSurveyHandler);
 
 export default handler;
