df1ef06d71551a80d953b4ca22821f9bdf577a68
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-04-27 15:48:47 +0900

    Fix incorrect schema validation in MCO

--- pages/api/multiplechoiceoption/index.ts
+++ pages/api/multiplechoiceoption/index.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import NextConnectHandler from 'next-connect';
 import requireSession from '../../../api/middleware/requireSession.middleware';
-import validateResource from '../../../api/middleware/validateResource.middleware';
 import { createMultipleChoiceOptionHandler } from '../../../api/multipleChoiceOption/multipleChoiceOption.controller';
-import { createDefaultMultipleChoiceOptionSchema } from '../../../api/multipleChoiceOption/multipleChoiceOption.schema';
 
 const handler = NextConnectHandler<NextApiRequest, NextApiResponse>()
-  .post(validateResource(createDefaultMultipleChoiceOptionSchema), requireSession, createMultipleChoiceOptionHandler);
+  // we're not using the request body here, so there is nothing to validate
+  .post(requireSession, createMultipleChoiceOptionHandler);
 
 export default handler;
