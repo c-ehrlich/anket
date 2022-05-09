409f6c5de0843ffd15ac711550b8ab82d0c73b06
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-09 16:36:54 +0200

    chore: Remove all references to Product

---
+++ prisma/migrations/20220509142526_dev15/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Product` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_creatorId_fkey";
+
+-- DropTable
+DROP TABLE "Product";
