e7200f992d000593bf4f06b4026ba3ed86ded5f4
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-15 22:20:03 +0200

    landing: Hero and Cards layout (desktop)

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -4,151 +4,135 @@ import {
   Title,
   Text,
   Button,
-  SimpleGrid,
   Center,
   Paper,
+  Box,
 } from '@mantine/core';
-import { useViewportSize } from '@mantine/hooks';
+import { useMediaQuery, useViewportSize } from '@mantine/hooks';
 import Image from 'next/image';
 import { signIn } from 'next-auth/react';
-import React, { useState } from 'react';
-import { ArrowBigRight, ArrowRightCircle } from 'tabler-icons-react';
+import React from 'react';
+import styles from './_styles/LandingPage.module.css';
 
 const features = [
   {
-    url: '/img/landing/irasutoya_computer1.png',
-    altText: '0',
-  },
-  {
+    title: 'Create surveys quickly',
+    content:
+      'Our efficient survey editor and prebuilt question types make it faster than ever to create survey',
     url: '/img/landing/irasutoya_computer1.png',
     altText: '1',
   },
   {
+    title: 'High completion rate',
+    content: 'Easy to understand and fun UI means fewer users quit mid-survey',
     url: '/img/landing/irasutoya_qa1.png',
     altText: '2',
   },
   {
+    title: 'Analyze your responses',
+    content:
+      'Use easy to understand graphs and statistics to optimize your business decisions',
     url: '/img/landing/irasutoya_chart1.png',
     altText: '3',
-  },
-  {
-    url: '/img/landing/irasutoya_cash1.png',
-    altText: '4',
   },
 ];
 
 const LandingPage = () => {
   const { width, height } = useViewportSize();
-  const [selectedFeature, setSelectedFeature] = useState<number>(0);
+  const sm = useMediaQuery('(max-width: 719px)');
 
   return (
-    <Center>
-      <Stack style={{ margin: '16px' }} spacing={64}>
-        <Group position='apart'>
-          <Image
-            src='/logo/logo-v01-black.png'
-            alt='anket-logo'
-            height='32px'
-            width='100%'
-          />
-          <Group>
-            <Button onClick={() => signIn()}>Sign in</Button>
-            <Button onClick={() => signIn()}>Get started</Button>
-          </Group>
+    <Stack>
+      <Group position='apart' className={styles.headerWrapper}>
+        <Image
+          src='/logo/logo-v01-black.png'
+          alt='anket-logo'
+          height='32px'
+          width='100%'
+        />
+        <Group>
+          <div onClick={() => signIn()} className={styles.loginButton}>
+            Sign in
+          </div>
+          <Button onClick={() => signIn()}>Get started</Button>
         </Group>
-        <Group grow={true}>
-          <Stack>
-            <Title order={1}>Get to know your customers!</Title>
-            <Text>
+      </Group>
+      <Center>
+        <div className={styles.container}>
+          <div className={styles.heroGrid}>
+            <Title order={1} className={styles.heroHeader}>
+              Get to know your customers!
+            </Title>
+            <Text className={styles.heroText}>
               One platform to create surveys and analyze results to find out how
               to make your company the best it can be.
             </Text>
-            <ul>
+            <ul className={styles.heroList}>
               <li>Simple to use</li>
               <li>Minimal admin required</li>
               <li>Clear results</li>
             </ul>
-            <Button onClick={() => signIn()}>Get started (free)</Button>
-          </Stack>
-          <Paper>
-            <Image
-              src='/img/landing/irasutoya_table1.png'
-              alt='landing-image-1'
-              width='100%'
-              height='100%'
-              layout='responsive'
-            />
-          </Paper>
-        </Group>
-        <Group grow={true}>
-          <SimpleGrid cols={2}>
-            <div
-              style={{
-                borderRadius: '16px',
-                border: '1px solid grey',
-                padding: '16px',
-              }}
-              onMouseEnter={() => setSelectedFeature(1)}
-              onClick={() => setSelectedFeature(1)}
+            <Button
+              className={styles.heroButton}
+              size='xl'
+              onClick={() => signIn()}
             >
-              <Title order={3}>Create surveys quickly</Title>
-              <Text>
-                Our efficient survey editor and prebuilt question types make it
-                faster than ever to create survey
-              </Text>
-            </div>
-            <div
-              style={{
-                borderRadius: '16px',
-                border: '1px solid grey',
-                padding: '16px',
-              }}
-              onMouseEnter={() => setSelectedFeature(2)}
-              onClick={() => setSelectedFeature(2)}
+              Get started (free)
+            </Button>
+            <Box className={styles.heroImage}>
+              <Image
+                src='/img/landing/irasutoya_table1.png'
+                alt='landing-image-1'
+                width='100%'
+                height='100%'
+                layout='responsive'
+              />
+            </Box>
+          </div>
+        </div>
+      </Center>
+      <Stack>
+        <Center>
+          <div className={styles.container}>
+            <Group
+              className={styles.cardGroup}
+              spacing={32}
+              style={{ margin: '16px' }}
+              align='stretch'
+              noWrap
+              grow={true}
+              direction={sm ? 'column' : 'row'}
             >
-              <Title order={3}>High completion rate</Title>
-              <Text>
-                Easy to understand and fun UI means fewer users quit mid-survey
-              </Text>
-            </div>
-            <div
-              style={{
-                borderRadius: '16px',
-                border: '1px solid grey',
-                padding: '16px',
-              }}
-              onMouseEnter={() => setSelectedFeature(3)}
-              onClick={() => setSelectedFeature(3)}
-            >
-              <Title order={3}>Analyze your responses</Title>
-              <Text>
-                Make the most of your survey response data with easy to
-                understand graphs and statistics
-              </Text>
-            </div>
-            <div
-              style={{
-                borderRadius: '16px',
-                border: '1px solid grey',
-                padding: '16px',
-              }}
-              onMouseEnter={() => setSelectedFeature(4)}
-              onClick={() => setSelectedFeature(4)}
-            >
-              <Title order={3}>Make better decisions</Title>
-              <Text>Use the results to optimize your business decisions</Text>
-            </div>
-          </SimpleGrid>
-          <Paper>
-            <Image
-              src={features[selectedFeature].url}
-              alt={features[selectedFeature].altText}
-              width='100%'
-              height='100%'
-              layout='responsive'
-            />
-          </Paper>
-        </Group>
+              {features.map((feature, index) => (
+                <Paper
+                  key={`feature-${index}`}
+                  shadow='md'
+                  withBorder
+                  p='xl'
+                  radius='sm'
+                >
+                  <Stack className={styles.card}>
+                    <div className={styles.cardImageWrapper}>
+                      <Image
+                        src={feature.url}
+                        alt={feature.altText}
+                        height='100%'
+                        width='100%'
+                        layout='responsive'
+                      />
+                    </div>
+                    <Title order={3} align='center'>
+                      {feature.title}
+                    </Title>
+                    <Text align='center' component='span'>
+                      {feature.content}
+                    </Text>
+                  </Stack>
+                </Paper>
+              ))}
+            </Group>
+          </div>
+        </Center>
         <Group grow={true}>
           <Paper>
             <Image
@@ -160,43 +144,51 @@ const LandingPage = () => {
             />
           </Paper>
           <Stack>
-            <Title order={2}>SurveyMonkey is broken. We fixed it.</Title>
-            <Text>
+            <Title className={styles.comparisonHeader} order={2}>
+              SurveyMonkey is broken. We fixed it.
+            </Title>
+            <Text className={styles.comparisonText}>
               {`Everything you need, nothing you don't need - create surveys,
             increase your response rates, analyze response data, and learn how
             to improve your business.`}
             </Text>
-            <Group>
-              <Button onClick={() => signIn()}>
-                <Group spacing={8}>
-                  <ArrowBigRight />
-                  <span>Get started</span>
-                </Group>
+            <Center>
+              <Button size='lg' onClick={() => signIn()}>
+                Get started
               </Button>
+            </Center>
+          </Stack>
+        </Group>
+        <Center className={styles.lastCtaBg}>
+          <div className={styles.container}>
+            <Group grow={true}>
+              <Stack align='center'>
+                <Title order={1} className={styles.lastCta}>
+                  Create your first survey now
+                </Title>
+                <Button size='xl' onClick={() => signIn()}>
+                  Let&apos;s do this
+                </Button>
+              </Stack>
+              <Box>
+                <Image
+                  src='/img/landing/irasutoya_genki1.png'
+                  alt='genki'
+                  width='100%'
+                  height='100%'
+                  layout='responsive'
+                />
+              </Box>
             </Group>
-          </Stack>
-        </Group>
-        <Group grow={true}>
-          <Stack>
-            <Title order={1}>Create your first survey now</Title>
-            <Button onClick={() => signIn()}>Get started (free)</Button>
-          </Stack>
-          <Paper>
-            <Image
-              src='/img/landing/irasutoya_genki1.png'
-              alt='genki'
-              width='100%'
-              height='100%'
-              layout='responsive'
-            />
-          </Paper>
-        </Group>
+          </div>
+        </Center>
         <Stack>
-          <hr style={{ width: '100%' }} />
-          <Center>Anket - Copyright {new Date().getFullYear()}</Center>
+          <div className={styles.footerText}>
+            Anket - Copyright {new Date().getFullYear()}
+          </div>
         </Stack>
       </Stack>
-    </Center>
+    </Stack>
   );
 };
 
