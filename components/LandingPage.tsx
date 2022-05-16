01d63a6416e0b551859f77a31a409c6fbafaed68
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-16 14:34:13 +0200

    landing: Hero and Scroll

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -7,12 +7,18 @@ import {
   Center,
   Paper,
   Box,
+  UnstyledButton,
 } from '@mantine/core';
-import { useMediaQuery, useViewportSize } from '@mantine/hooks';
+import {
+  useMediaQuery,
+  useViewportSize,
+  useWindowScroll,
+} from '@mantine/hooks';
 import Image from 'next/image';
 import { signIn } from 'next-auth/react';
 import React from 'react';
 import styles from './_styles/LandingPage.module.css';
+import { ArrowDown } from 'tabler-icons-react';
 
 const features = [
   {
@@ -23,7 +29,7 @@ const features = [
     altText: '1',
   },
   {
-    title: 'High completion rate',
+    title: 'High survey completion rate',
     content: 'Easy to understand and fun UI means fewer users quit mid-survey',
     url: '/img/landing/irasutoya_qa1.png',
     altText: '2',
@@ -38,112 +44,156 @@ const features = [
 ];
 
 const LandingPage = () => {
-  const { width, height } = useViewportSize();
+  const [scroll, scrollTo] = useWindowScroll();
+  const viewport = useViewportSize();
   const sm = useMediaQuery('(max-width: 719px)');
 
   return (
-    <Stack>
-      <Group position='apart' className={styles.headerWrapper}>
-        <Image
-          src='/logo/logo-v01-black.png'
-          alt='anket-logo'
-          height='32px'
-          width='100%'
-        />
-        <Group>
-          <div onClick={() => signIn()} className={styles.loginButton}>
-            Sign in
-          </div>
-          <Button onClick={() => signIn()}>Get started</Button>
+    <Stack spacing={0}>
+      <Box style={{ height: Math.min(viewport.height, 900) }}>
+        {/* NAVBAR */}
+        <Group position='apart' className={styles.headerWrapper}>
+          <Image
+            src='/logo/logo-v01-black.png'
+            alt='anket-logo'
+            height='32px'
+            width='100%'
+          />
+          <Group>
+            <div onClick={() => signIn()} className={styles.loginButton}>
+              Sign in
+            </div>
+            <Button onClick={() => signIn()}>Get started</Button>
+          </Group>
         </Group>
-      </Group>
+
+        {/* HERO */}
+        <Center style={{ height: '100%' }}>
+          <div className={styles.container} style={{ marginBottom: '96px' }}>
+            <div className={styles.heroGrid}>
+              <Title order={1} className={styles.heroHeader}>
+                Get to know your customers!
+              </Title>
+              <Text className={styles.heroText}>
+                One platform to create surveys and analyze results to find out
+                how to make your company the best it can be.
+              </Text>
+              <ul className={styles.heroList}>
+                <li>Simple to use</li>
+                <li>Minimal admin required</li>
+                <li>Clear results</li>
+              </ul>
+              <Button
+                className={styles.heroButton}
+                size='xl'
+                onClick={() => signIn()}
+              >
+                Get started (free)
+              </Button>
+              <Box className={styles.heroImage}>
+                <Image
+                  src='/img/landing/irasutoya_table1.png'
+                  alt='landing-image-1'
+                  width='100%'
+                  height='100%'
+                  layout='responsive'
+                />
+              </Box>
+            </div>
+          </div>
+        </Center>
+      </Box>
+
+      {/* KEEP SCROLLING */}
+      {/* TODO position this relative to the entire window
+          and make it scroll to the top of the next section */}
+      <div style={{ width: '0', height: '0', position: 'relative' }}>
+        {scroll.y === 0 && (
+          <Center
+            style={{ width: '100vw', position: 'absolute', bottom: '32px' }}
+          >
+            <UnstyledButton
+              onClick={() => scrollTo({ y: Math.min(900, viewport.height) })}
+            >
+              <Center className={styles.downButtonBG}>
+                <ArrowDown color='white' />
+              </Center>
+            </UnstyledButton>
+          </Center>
+        )}
+      </div>
+
+      {/* WHY ANKET */}
+      <Center className={styles.whyAnketBG}>
+        <Stack align='center' className={styles.container}>
+          <div>
+            <Title order={2} className={styles.whyAnketHeader}>
+              Why Anket?
+            </Title>
+            <Text className={styles.whyAnketText} align='center'>
+              {`We're built for growing businesses and nonprofits. Put our survey
+            and data analysis tools behind your idea, dream, brand, or business
+            and we'll help you make the best decisions.`}
+            </Text>
+          </div>
+        </Stack>
+
+        {/* CARDS */}
+        <div className={styles.container}>
+          <Group
+            className={styles.cardGroup}
+            spacing={32}
+            style={{ margin: '16px' }}
+            align='stretch'
+            noWrap
+            grow={true}
+            direction={sm ? 'column' : 'row'}
+          >
+            {features.map((feature, index) => (
+              <Paper
+                className={styles.cardOuter}
+                key={`feature-${index}`}
+                shadow='md'
+                withBorder
+                p='xl'
+                radius='sm'
+              >
+                <Stack className={styles.card}>
+                  <div className={styles.cardImageWrapper}>
+                    <Image
+                      src={feature.url}
+                      alt={feature.altText}
+                      height='100%'
+                      width='100%'
+                      layout='responsive'
+                    />
+                  </div>
+                  <Title order={3} align='center'>
+                    {feature.title}
+                  </Title>
+                  <Text align='center' component='span'>
+                    {feature.content}
+                  </Text>
+                </Stack>
+              </Paper>
+            ))}
+          </Group>
+        </div>
+      </Center>
+
+      {/* COMPARISON */}
       <Center>
         <div className={styles.container}>
-          <div className={styles.heroGrid}>
-            <Title order={1} className={styles.heroHeader}>
-              Get to know your customers!
-            </Title>
-            <Text className={styles.heroText}>
-              One platform to create surveys and analyze results to find out how
-              to make your company the best it can be.
-            </Text>
-            <ul className={styles.heroList}>
-              <li>Simple to use</li>
-              <li>Minimal admin required</li>
-              <li>Clear results</li>
-            </ul>
-            <Button
-              className={styles.heroButton}
-              size='xl'
-              onClick={() => signIn()}
-            >
-              Get started (free)
-            </Button>
-            <Box className={styles.heroImage}>
+          <div className={styles.comparisonGrid}>
+            <Box className={styles.comparisonImage}>
               <Image
-                src='/img/landing/irasutoya_table1.png'
-                alt='landing-image-1'
+                src='/img/landing/irasutoya_board1.png'
+                alt='race1'
                 width='100%'
                 height='100%'
                 layout='responsive'
               />
             </Box>
-          </div>
-        </div>
-      </Center>
-      <Stack>
-        <Center>
-          <div className={styles.container}>
-            <Group
-              className={styles.cardGroup}
-              spacing={32}
-              style={{ margin: '16px' }}
-              align='stretch'
-              noWrap
-              grow={true}
-              direction={sm ? 'column' : 'row'}
-            >
-              {features.map((feature, index) => (
-                <Paper
-                  key={`feature-${index}`}
-                  shadow='md'
-                  withBorder
-                  p='xl'
-                  radius='sm'
-                >
-                  <Stack className={styles.card}>
-                    <div className={styles.cardImageWrapper}>
-                      <Image
-                        src={feature.url}
-                        alt={feature.altText}
-                        height='100%'
-                        width='100%'
-                        layout='responsive'
-                      />
-                    </div>
-                    <Title order={3} align='center'>
-                      {feature.title}
-                    </Title>
-                    <Text align='center' component='span'>
-                      {feature.content}
-                    </Text>
-                  </Stack>
-                </Paper>
-              ))}
-            </Group>
-          </div>
-        </Center>
-        <Group grow={true}>
-          <Paper>
-            <Image
-              src='/img/landing/irasutoya_board1.png'
-              alt='race1'
-              width='100%'
-              height='100%'
-              layout='responsive'
-            />
-          </Paper>
-          <Stack>
             <Title className={styles.comparisonHeader} order={2}>
               SurveyMonkey is broken. We fixed it.
             </Title>
@@ -152,41 +202,50 @@ const LandingPage = () => {
             increase your response rates, analyze response data, and learn how
             to improve your business.`}
             </Text>
-            <Center>
-              <Button size='lg' onClick={() => signIn()}>
-                Get started
+            <ul className={styles.comparisonList}>
+              <li>Free for personal and nonprofit use</li>
+              <li>Intuitive website</li>
+              <li>Easy access to response statistics</li>
+            </ul>
+            <Button
+              className={styles.comparisonButton}
+              size='lg'
+              onClick={() => signIn()}
+            >
+              Get started
+            </Button>
+          </div>
+        </div>
+      </Center>
+      <Center className={styles.lastCtaBg}>
+        <div className={styles.container}>
+          <Group grow={true}>
+            <Stack align='center'>
+              <Title order={1} className={styles.lastCta}>
+                Create your first survey now
+              </Title>
+              <Button size='xl' onClick={() => signIn()}>
+                Let&apos;s do this
               </Button>
-            </Center>
-          </Stack>
-        </Group>
-        <Center className={styles.lastCtaBg}>
-          <div className={styles.container}>
-            <Group grow={true}>
-              <Stack align='center'>
-                <Title order={1} className={styles.lastCta}>
-                  Create your first survey now
-                </Title>
-                <Button size='xl' onClick={() => signIn()}>
-                  Let&apos;s do this
-                </Button>
-              </Stack>
-              <Box>
-                <Image
-                  src='/img/landing/irasutoya_genki1.png'
-                  alt='genki'
-                  width='100%'
-                  height='100%'
-                  layout='responsive'
-                />
-              </Box>
-            </Group>
-          </div>
-        </Center>
-        <Stack>
-          <div className={styles.footerText}>
-            Anket - Copyright {new Date().getFullYear()}
-          </div>
-        </Stack>
+            </Stack>
+            <Box>
+              <Image
+                src='/img/landing/irasutoya_genki1.png'
+                alt='genki'
+                width='100%'
+                height='100%'
+                layout='responsive'
+              />
+            </Box>
+          </Group>
+        </div>
+      </Center>
+
+      {/* FOOTER */}
+      <Stack>
+        <div className={styles.footerText}>
+          Anket - Copyright {new Date().getFullYear()}
+        </div>
       </Stack>
     </Stack>
   );
