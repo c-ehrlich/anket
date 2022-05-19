d1140226f294a51adf8544104095c0078354362b
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-19 13:21:41 +0200

    Remove outdated comments

--- api/surveyParticipation/surveyParticipation.controller.ts
+++ api/surveyParticipation/surveyParticipation.controller.ts
@@ -1,21 +1,9 @@
-// next: create the controller that either creates or gets a survey interaction
-// we need:
-// surveyId: from url
-// userId: from next-auth
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import getId from '../utils/getId';
 import logger from '../utils/logger';
-import {
-  SurveyWithParticipationAndUserResponses,
-} from './surveyParticipation.schema';
+import { SurveyWithParticipationAndUserResponses } from './surveyParticipation.schema';
 import { getOrCreateSurveyParticipation } from './surveyParticipation.service';
-
-// get surveyId
-// get userId
-// service checks if one that meets those exists, if so gets it and all questionResponses
-// if not, create it and return it
 
 export async function getOrCreateSurveyParticipationHandler(
   req: NextApiRequest,
