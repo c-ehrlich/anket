9d7c0a17e0d1ab615d180f8f541836fc868821ed
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-26 13:34:03 +0200

    Put env vars in next.config AND deploy.yml

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-  experimental: {
-    reactRoot: true
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+    // reactStrictMode: true,
+    experimental: {
+      reactRoot: true,
+    },
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      DISCORD_ID: process.env.DISCORD_ID,
+      DISCORD_SECRET: process.env.DISCORD_SECRET,
+      GITHUB_ID: process.env.GITHUB_ID,
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    }
   }
+  return nextConfig
 }
-
-module.exports = nextConfig
