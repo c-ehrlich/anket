dab643e23e45a333bbb9a3dd078b282984bc8136
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-26 20:33:52 +0200

    Remove unnecessary stuff from NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
+// import GoogleProvider from 'next-auth/providers/google';
 import prisma from '../../../api/utils/prisma';
 
 export default NextAuth({
@@ -17,15 +17,16 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    })
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // })
     // ...add more providers here
   ],
-  pages: {
-    signIn: '/signin',
-  },
+  // TODO switch back to default signin page
+  // pages: {
+  //   signIn: '/signin',
+  // },
   callbacks: {
     async session({ session, user }) {
       if (session?.user) {
@@ -34,5 +35,4 @@ export default NextAuth({
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
 });
