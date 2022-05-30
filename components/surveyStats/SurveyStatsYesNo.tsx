8d4f2cab47c4a2d2c345367133e44a08b52d80bd
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-05-30 18:49:04 +0200

    remove unused import

--- components/surveyStats/SurveyStatsYesNo.tsx
+++ components/surveyStats/SurveyStatsYesNo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { SurveyStatsResponseQuestion } from '../../backend/surveyStats/surveyStats.schema';
 import { GradientLightgreenGreen } from '@visx/gradient';
 import { Group } from '@visx/group';
