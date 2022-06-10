473ca846c76740784b07db0e04a2b18a042ee648
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-10 15:03:45 +0200

    Clean up validateResource

--- backend/middleware/validateResource.middleware.ts
+++ backend/middleware/validateResource.middleware.ts
@@ -1,11 +1,6 @@
-// a curried function that takes the schema we created with Zod and validates
-// a request against that schema
-// so when we get to our controller, we know that the body is exactly what we expect
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextHandler } from 'next-connect';
 import { AnyZodObject } from 'zod';
-import logger from '../utils/logger';
 
 const validateResource =
   (schema: AnyZodObject) =>
@@ -14,12 +9,14 @@ const validateResource =
       schema.parse({
         body: req.body,
         query: req.query,
+        // TODO create zod typings that also validate params for requests where it's necessary
+        // until that is implemented, trying to validate params will _always_ throw an error
         // params: req.params,
       });
       next();
     } catch (e: any) {
       // if our schema can not be parsed
-      return res.status(400).send(e.errors);
+      return res.status(400).send(e.hasOwnProperty('errors') ? e.errors : e);
     }
   };
 
